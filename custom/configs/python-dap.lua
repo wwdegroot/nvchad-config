030ad4f8165b3355df7445ca1bae534da60121c8
wwdegroot <wouter.w.de.groot@gmail.com>
2024-08-08 13:29:53 +0200

    old config removed

--- custom/configs/python-dap.lua
+++
@@ -1,7 +0,0 @@
-local mason_registry = require("mason-registry")
-
-local codelldb = mason_registry.get_package("debugpy")
-local venv_path = codelldb:get_install_path()
-local python_path = extension_path .. "venv\\Scripts\\python.exe"
-
-return python_path
