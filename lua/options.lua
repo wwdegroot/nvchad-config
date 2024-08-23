78fa10f242f76161426207b678188fde2a30f779
wwdegroot <wouter.w.de.groot@gmail.com>
2024-08-23 21:51:49 +0200

    pwsh als default shell in neovim

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,16 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.o.shell = "pwsh.exe"
 
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+
+vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+
+vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+
+vim.o.shellquote = ""
+
+vim.o.shellxquote = ""
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
