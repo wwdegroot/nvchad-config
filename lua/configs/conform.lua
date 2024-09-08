c75f5e8a761c43bdcb493d866d31f3705991eeb2
wwdegroot <wouter.w.de.groot@gmail.com>
2024-09-08 12:03:40 +0200

    conform extra formatters voor css, html, go, rust, python

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,24 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    go = { "goimports", "gofmt" },
+    rust = { "rustfmt", lsp_format = "fallback" },
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+     -- These options will be passed to conform.format()
+     timeout_ms = 500,
+     lsp_fallback = true,
+  },
 }
 
 return options
