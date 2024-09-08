485e803ba2449d6ac2689de8f0f7c6200315f025
wwdegroot <wouter.w.de.groot@gmail.com>
2024-09-08 13:00:05 +0200

    fix voor tsserver -> ts_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -15,6 +15,10 @@ local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
+  -- workaround for tsserver transition to ts_ls https://github.com/neovim/nvim-lspconfig/pull/3232#issuecomment-2331025714
+  if lsp == "tsserver" then
+    lsp = "ts_ls"
+  end
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
