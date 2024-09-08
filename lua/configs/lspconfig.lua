60ea3c83c968cd205047a562dda18a89d0ca1e12
wwdegroot <wouter.w.de.groot@gmail.com>
2024-09-08 22:12:10 +0200

    yamlls configuration

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -32,3 +32,21 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+-- yaml lsp config
+lspconfig.yamlls.setup{
+  settings = {
+    yaml = {
+      schemas = {
+        kubernetes = "k8s-*.yaml",
+        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
+        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+        ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+      },
+    },
+  },
+}
